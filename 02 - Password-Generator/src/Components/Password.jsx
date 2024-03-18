e5ed52b3d7f8236d36f3e086f77082eb827894d9
biswaljyotirmaya <biswaljyotirmaya06@gmail.com>
2024-03-18 12:10:53 +0530

    Used hooks to make it functional

--- 02 - Password-Generator/src/Components/Password.jsx
+++ 02 - Password-Generator/src/Components/Password.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { MdOutlineContentCopy } from "react-icons/md";
 import Select from "./Select";
 
 function Password() {
+  const [length, setLength] = useState(8);
+  const [uppercaseAllowed, setUppercaseAllowed] = useState(false);
+  const [lowercaseAllowed, setLowercaseAllowed] = useState(true);
+  const [numbersAllowed, setNumbersAllowed] = useState(false);
+  const [symbolsAllowed, setSymbolsAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const generatePassword = useCallback(() => {
+    let str = "";
+    let pass = "";
+    if (uppercaseAllowed) str += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (lowercaseAllowed) str += "abcdefghijklmnopqustuvwxyz";
+    if (numbersAllowed) str += "0123456789";
+    if (symbolsAllowed) str += "!@#$%^&*()_+";
+
+    for (let i = 1; i < length; i++) {
+      const char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+    console.log(password);
+  }, [
+    length,
+    uppercaseAllowed,
+    lowercaseAllowed,
+    numbersAllowed,
+    symbolsAllowed,
+  ]);
+
   return (
     <div className="bg-[#0a0e31] w-96 rounded-xl p-6 text-white">
       <h2 className="text-3xl font-semibold my-4">Password Generator</h2>
@@ -12,7 +41,7 @@ function Password() {
           name=""
           id=""
           placeholder="Password"
-          value={""}
+          value={password}
           readOnly
           className="text-orange-700 bg-[#1D2141] text-lg px-4 outline-none  h-10 rounded-lg w-[80%] mr-8 overflow-hiddens"
         />
@@ -21,7 +50,7 @@ function Password() {
         </button>
       </div>
 
-      <p className="mt-2">Length:</p>
+      <p className="mt-2">Length: {length}</p>
 
       <div className="flex justify-evenly  items-center h-12 bg-[#1D2141] rounded px-6">
         <p>6</p>
@@ -29,21 +58,42 @@ function Password() {
           type="range"
           name=""
           id=""
+          defaultValue={length}
           min={6}
           max={32}
           className="w-full mx-2 outline-none"
+          onChange={(e) => setLength(e.target.value)}
         />
         <p>32</p>
       </div>
       <p className="mt-2 -mb-2">Settings</p>
       <div>
-        <Select tag={"Include Uppercase"} />
-        <Select tag={"Include Lowercase"} />
-        <Select tag={"Include Numbers"} />
-        <Select tag={"Include Symbols"} />
+        <Select
+          tag={"Include Uppercase"}
+          defultChecked={uppercaseAllowed}
+          setCheck={setUppercaseAllowed}
+        />
+        <Select
+          tag={"Include Lowercase"}
+          defultChecked={lowercaseAllowed}
+          setCheck={setLowercaseAllowed}
+        />
+        <Select
+          tag={"Include Numbers"}
+          defultChecked={numbersAllowed}
+          setCheck={setNumbersAllowed}
+        />
+        <Select
+          tag={"Include Symbols"}
+          defultChecked={symbolsAllowed}
+          setCheck={setSymbolsAllowed}
+        />
       </div>
 
-      <button className="text-center bg-gradient-to-r from-sky-500 to-indigo-500  h-12 w-full my-2 rounded font-semibold text-xl transition hover:from-[#1D2141] hover:to-[#1D2141] duration-2000">
+      <button
+        className="text-center bg-gradient-to-r from-sky-500 to-indigo-500  h-12 w-full my-2 rounded font-semibold text-xl transition hover:from-[#545fb1] hover:to-[#303c97] hover:text-[#6478fa] duration-2000 "
+        onClick={generatePassword}
+      >
         Generate Password
       </button>
     </div>
--- 02 - Password-Generator/src/Components/Select.jsx
+++ 02 - Password-Generator/src/Components/Select.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
-function Select({ tag }) {
+function Select({ tag, defultChecked, setCheck }) {
   return (
     <div className="flex justify-between items-center px-6 bg-[#1D2141] h-12 my-2 rounded">
-      <label htmlFor={tag} className="cursor-pointer text-white text-lg font-semibold">
+      <label
+        htmlFor={tag}
+        className="cursor-pointer text-white text-lg font-semibold"
+      >
         {tag}
       </label>
-      <input type="checkbox" id={tag} className="h-4 w-4 cursor-pointer" />
+      <input
+        type="checkbox"
+        checked={defultChecked}
+        id={tag}
+        className="h-4 w-4 cursor-pointer"
+        onChange={() => {
+          setCheck((prev) => !prev);
+        }}
+      />
     </div>
   );
 }
