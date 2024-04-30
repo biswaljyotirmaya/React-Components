ecab00fc4a53bbf24280b1bb0bd68b1682ea5d86
biswaljyotirmaya <biswaljyotirmaya06@gmail.com>
2024-04-30 15:13:34 +0530

    Updated Converter

--- 03-Currency-Conveter/src/App.jsx
+++ 03-Currency-Conveter/src/App.jsx
@@ -5,22 +5,18 @@ import useCurrencyInfo from "./hooks/useCurrencyInfo";
 function App() {
   const [from, setFrom] = useState("inr");
   const [to, setTo] = useState("usd");
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState(null);
   const [convertedAmount, setConvertedAmount] = useState(0);
-  const [options, setOptions] = useState([]);
+
   const currencyInfo = useCurrencyInfo(from);
 
-  useEffect(() => {
-    setOptions(Object.keys(currencyInfo));
-  }, [currencyInfo]);
-
-  useEffect(() => {
-    convert();
-  }, [amount,from, to, currencyInfo]);
+  const options = Object.keys(currencyInfo);
 
   const swap = () => {
     setFrom(to);
     setTo(from);
+    setAmount(convertedAmount);
+    setConvertedAmount(0);
   };
 
   const convert = () => {
@@ -28,47 +24,51 @@ function App() {
   };
 
   return (
-    <div className="h-screen w-full bg-[#401570] flex justify-center items-center">
-      <div className="bg-[#ffffff15] p-8 rounded-lg shadow-md shadow-black">
-        <p className="text-center text-4xl font-bold text-white -mt-2">
-          Currency Conveter
-        </p>
-        <p className="text-center text-orange-500 text-lg font-semibold my-2">
-          {" "}
-          1 {from.toUpperCase()} = {currencyInfo[to]} {to.toUpperCase()}
-        </p>
-        <InputCurrency
-          readOnly={false}
-          amount={amount}
-          selectCurrency={from}
-          currencyOptions={options}
-          setAmount={setAmount}
-          setFrom={setFrom}
-          setTo={setTo}
-          set={true}
-        />
-        <button
-          className="text-white text-center bg-violet-900 hover:bg-violet-700 px-8 py-2 font-bold text-lg rounded-3xl absolute top-[47.5%] left-[47%] shadow-md shadow-black"
-          onClick={swap}
-        >
-          Swap
-        </button>
-        <InputCurrency
-          readOnly={true}
-          amount={convertedAmount}
-          selectCurrency={to}
-          currencyOptions={options}
-          setAmount={setAmount}
-          setFrom={setFrom}
-          setTo={setTo}
-          set={false}
-        />
-        <button
-          className="bg-[#401570] hover:shadow-md hover:shadow-black w-full py-4 text-xl font-bold rounded-md text-white"
-          onClick={convert}
-        >
-          Convert {from.toUpperCase()} to {to.toUpperCase()}
-        </button>
+    <div className="h-screen w-full flex bg-white">
+      <div className="h-screen w-[50%] bg-white flex items-center justify-center">
+        <img src="/exchange.jpg" alt="" className="w-full h-screen" />
+      </div>
+      <div className=" w-[50%]  flex flex-col justify-center items-center  bg-[#0057ff]">
+        <div className="border-[3px] border-white p-10 rounded-xl relative">
+          <div>
+            <p className="text-center text-4xl font-bold text-white mt-2">
+              Currency Conveter
+            </p>
+            <p className="text-center text-black text-lg font-semibold my-2">
+              {" "}
+              1 {from.toUpperCase()} = {currencyInfo[to]} {to.toUpperCase()}
+            </p>
+
+            <InputCurrency
+              label="From"
+              amount={amount}
+              currencyOptions={options}
+              onCurrencyChange={(currency) => setFrom(currency)}
+              selectCurrency={from}
+              onAmountChange={(amount) => setAmount(amount)}
+            />
+            <button
+              className="text-[#0057ff] text-center bg-white border-2 border-black hover:shadow-md px-8 py-2 font-bold text-lg rounded-3xl absolute top-[47.5%] left-[40%]  hover:shadow-black"
+              onClick={swap}
+            >
+              Swap
+            </button>
+            <InputCurrency
+              label="To"
+              amount={convertedAmount}
+              currencyOptions={options}
+              onCurrencyChange={(currency) => setTo(currency)}
+              selectCurrency={to}
+              amountDisable
+            />
+            <button
+              className="border-2 hover:shadow-md hover:shadow-black w-full py-4 text-xl font-bold rounded-md text-white"
+              onClick={convert}
+            >
+              Convert {from.toUpperCase()} to {to.toUpperCase()}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- 03-Currency-Conveter/src/Components/InputCurrency.jsx
+++ 03-Currency-Conveter/src/Components/InputCurrency.jsx
@@ -2,63 +2,54 @@ import React from "react";
 import { useId } from "react";
 
 function InputCurrency({
+  label,
   amount,
-  readOnly,
+  onAmountChange,
+  onCurrencyChange,
   currencyOptions = [],
-  selectCurrency = "inr",
+  selectCurrency = "usd",
+  amountDisable = false,
+  currencyDisable = false,
   className = "",
-  setAmount,
-  setFrom,
-  setTo,
-  set,
 }) {
-  const amountInputId = useId(); 
-  const handleAmountChange = (e) => {
-    setAmount(e.target.value);
-  };
-
-  const handleCurrencyChange = (e) => {
-    const selectedCurrency = e.target.value;
-    if (set) {
-      setFrom(selectedCurrency);
-    } else {
-      setTo(selectedCurrency);
-    }
-  };
+  const amountInputId = useId();
 
   return (
     <div
-      className={`w-[500px]  bg-[#401570] rounded-md my-4 flex  justify-between p-6 text-white pb-8 ${className}`}
+      className={`w-[500px]  bg-[#0057ff] rounded-md my-4 flex border border-white  justify-between p-6 text-white pb-8 ${className}`}
     >
       <div className="flex flex-col items-start">
-        <label className="text-lg mb-4" htmlFor={amountInputId}>
-          Amount
+        <label
+          htmlFor={amountInputId}
+          className="text-white mb-4 text-lg inline-block"
+        >
+          {label}
         </label>
+
         <input
-        readOnly={readOnly}
           id={amountInputId}
+          className="text-black bg-gray-200 p-2 outline-none rounded-md text-lg font-semibold h-10"
           type="number"
           placeholder="Amount"
+          disabled={amountDisable}
           value={amount}
-          min={0}
-          className="text-black bg-gray-200 p-2 outline-none rounded-md text-lg font-semibold h-10"
-          onChange={handleAmountChange} 
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
         />
       </div>
       <div className="flex flex-col items-end">
         <p className="mb-4 text-lg ">Currency Type</p>
+
         <select
-          value={selectCurrency}
-          onChange={handleCurrencyChange}  
           className="outline-none h-10 px-4 p-2 text-lg font-semibold  bg-gray-200 text-black rounded-full cursor-pointer"
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          disabled={currencyDisable}
         >
           {currencyOptions.map((currency) => (
-            <option
-              key={currency}
-              value={currency}
-              className="text-center py-2"
-            >
-              {currency && currency.toUpperCase()}
+            <option key={currency} value={currency}>
+              {currency.toUpperCase()}
             </option>
           ))}
         </select>
